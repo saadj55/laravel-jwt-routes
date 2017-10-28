1d7d9882a499fcc968a650f0049ae738133a4d6f
Saad Nasir <sa>
2017-10-29 04:20:28 +0500

    push progress

---
+++ src/RouteServiceProvider.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Saad\LaravelJWT;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+
+}
